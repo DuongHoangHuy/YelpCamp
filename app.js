c2bfbb51f80bbffc1b3e65e4e51d71489f296ebc
huy <huydh.tech@gmail.com>
2021-09-21 16:26:49 +0700

    use helmet

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const MongoStore = require('connect-mongo');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
 const app = express()
 
 //I__________________CONNECTION______________________
@@ -51,12 +52,14 @@ const store = new MongoStore({
 })
 
 const sessionConfig = {
+    name: 'blaahhh', // For hide the sesion
     store,
     secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true, //HTPPS
         expires: Date.now()+1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
     }
@@ -64,7 +67,64 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 // Securtity: NO SQL injection
-app.use(mongoSanitize());
+app.use(mongoSanitize())
+
+app.use(helmet())
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit.fontawesome.com/",
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdnjs.cloudflare.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+    "https://ka-f.fontawesome.com/"
+];
+
+const fontSrcUrls = [
+    "https://ka-f.fontawesome.com/"
+]
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'unsafe-inline'","'self'" , ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/project-storage/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
 //__________________PASSPORT______________
 app.use(passport.initialize())
 app.use(passport.session())
@@ -95,7 +155,7 @@ app.all('*', (req, res)=>{
 
 app.use((err, req, res, next)=>{
     const {message = 'Something went wrong', statusCode = 500} = err
-    res.status(statusCode).render('error', {message, statusCode})
+    res.status(statusCode).render('error', {message, statusCode, err})
 })
 
 const port = process.env.PORT || 3000
